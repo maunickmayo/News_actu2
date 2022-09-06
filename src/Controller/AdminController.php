2152b18391b590dc345c7794f6c4495751bd1789
jO mAY <104192198+Jomay23@users.noreply.github.com>
2022-09-06 20:52:12 +0200

    create_admin-controller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord", name="show_dashboard", methods={"GET"})
+     */
+    public function showDashboard(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render("admin/show_dashboard.html.twig");
+    }
+
+
+    
+     public function createArticle(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+     {
+       
+
+     }
+
+
+
+}
