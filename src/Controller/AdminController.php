c23ac4b335284f50ac26630c145145861d99a3dd
jO mAY <104192198+Jomay23@users.noreply.github.com>
2022-09-13 14:20:22 +0200

    Update admin + role hierarchy+access control

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -32,14 +32,16 @@ class AdminController extends AbstractController
         # (la première se trouve dans "access control" -> config/packages/security.yaml)
         // Ce bloc de code vous permet de vérifier si le rôle du user est ADMIN, sinon cela lance une
         // une erreur, qui est attrapée dans le catch et cela redirige avec un message dans une partie
-        // autorisée pour les différents rôles.
+        // autorisée pour les différents rôles. et apres faites la hiérachie des roles en identant correctement avec l'acces_control
+        /* role_hierarchy:    
+                ROLE_ADMIN: ROLE_USER   */
 
-        /*try {
+        try {
             $this->denyAccessUnlessGranted('ROLE_ADMIN');
         } catch (AccessDeniedException $exception) {
             $this->addFlash('warning', 'Cette partie du site est réservée aux admins');
             return $this->redirectToRoute('default_home');
-        }*/
+        }
 
         $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
         $categories = $entityManager->getRepository(Category::class)->findAll();
@@ -187,6 +189,55 @@ class AdminController extends AbstractController
         return $this->redirectToRoute('show_dashboard');
     }# end function softDelete
 
+    
+    /**
+     * @Route("/restaurer-un-article_{id}", name="restore_article", methods={"GET"})
+     */
+    public function restoreArticle(Article $article, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $article->setDeletedAt(null);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        $this->addFlash('success', "L'article a bien été restauré");
+        return $this->redirectToRoute('show_dashboard');
+    }
+
    
+    /**
+     * @Route("/voir-les-articles-archives", name="show_trash", methods={"GET"})
+     */
+    public function showTrash(EntityManagerInterface $entityManager): Response
+    {                          // (Article $article) on les recuo deouis la bdd et non des independances
+                               // slide (on l app ascensseur, gitignore /public/uploads/) 
+        $archivedArticles = $entityManager->getRepository(Article::class)->findByTrash();
+
+        return $this->render("admin/trash/article_trash.html.twig", [
+            'archivedArticles' => $archivedArticles
+        ]);
+    }
+
+    /**
+     * @Route("/supprimer-un-article_{id}", name="hard_delete_article", methods={"GET"})
+     */
+    public function hardDeleteArticle(Article $article, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        // Suppression manuelle de la photo
+        $photo = $article->getPhoto();
+
+        // On utilise la fonction native de PHP unlink() pour supprimer un fichier dans le filesystem(système de fichiers).
+        if($photo) {
+            unlink($this->getParameter('uploads_dir'). '/' . $photo); 
+            // pour supprimer en PHP unset -> variables et unlink ->fichiers.
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $this->addFlash('success', "L'article a bien été supprimé de la base de données");
+        return $this->redirectToRoute('show_trash');
+    }
 
 } # end class
+# difference rRedirectResponse et Response
