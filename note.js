f4813c9242ededb58e447d77f4ca99a6bb6bcc89
Maunick MAYO <104192198+maunickmayo@users.noreply.github.com>
2024-09-24 18:49:31 +0200

    Update note.js

--- note.js
+++ note.js
@@ -1,9 +1,1 @@
-//NB :le dossier build va contenir tout ce qu'il va etre construit la ligne de commande run dev
-// tt ce qu il y a ds public c est pr le navigateur
 
-/*<style>
-.example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-.example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>*/
-
-// home.html.twig  : c est la landing page (la racine de notre site)
